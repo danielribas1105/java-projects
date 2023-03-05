5d8e7a0099c64bf81cf7c5e623feb121b2bd7ef3
Daniel <danielribas1105@gmail.com>
2023-03-05 12:04:24 -0300

    create first java program

---
+++ src/br/com/drcode/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.drcode;
+
+import br.com.drcode.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world!");
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+	}
+
+}
