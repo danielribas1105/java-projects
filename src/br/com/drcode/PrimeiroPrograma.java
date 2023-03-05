cfd98ad1591303d66d44a926ed3c926bedf505f3
Daniel <danielribas1105@gmail.com>
2023-03-05 12:24:31 -0300

    first java program eclipse

--- src/br/com/drcode/PrimeiroPrograma.java
+++ src/br/com/drcode/PrimeiroPrograma.java
@@ -6,7 +6,9 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, world!");
+		int a = 3;
+		int b = 2;
+		System.out.println("Hello, world! " + (a + b));
 		
 		Gato gato = new Gato();
 		
