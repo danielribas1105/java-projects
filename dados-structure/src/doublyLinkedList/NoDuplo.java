7749aa1455e6c8df74fb5f1b92782f7db75bf446
Daniel <danielribas1105@gmail.com>
2023-03-15 11:23:22 -0300

    update class noduplo

--- dados-structure/src/doublyLinkedList/NoDuplo.java
+++ dados-structure/src/doublyLinkedList/NoDuplo.java
@@ -16,4 +16,29 @@ public class NoDuplo<T> {
         this.noPosterior = null;
         this.object = object;
     }
+
+    public NoDuplo(T object, NoDuplo<T> noAnterior, NoDuplo<T> noPosterior) {
+        this.object = object;
+        this.noAnterior = noAnterior;
+        this.noPosterior = noPosterior;
+    }
+
+    public T getObject() {return object;}
+
+    public void setObject(T object) {this.object = object;}
+
+    public NoDuplo<T> getNoAnterior() {return noAnterior;}
+
+    public void setNoAnterior(NoDuplo<T> noAnterior) {this.noAnterior = noAnterior;}
+
+    public NoDuplo<T> getNoPosterior() {return noPosterior;}
+
+    public void setNoPosterior(NoDuplo<T> noPosterior) {this.noPosterior = noPosterior;}
+
+    @Override
+    public String toString() {
+        return "NoDuplo{" +
+                "object=" + object +
+                '}';
+    }
 }
